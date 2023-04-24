68c1d2e71f29fc1e0d927cedd1fa92ad42934152
Administrator <kristofer.souza2027@hotmail.com>
2023-04-23 17:17:20 -0700

    Ajustes dao novo banco de dados MySQL

--- ProjetoDjando/projeto_cad_materiais/projeto_cad_materiais/settings.py
+++ ProjetoDjando/projeto_cad_materiais/projeto_cad_materiais/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'projeto_cad_materiais.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'estoque',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 

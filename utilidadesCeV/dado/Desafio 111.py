b845246c621e9ffecb40bc9b7ced94ed86debe22
Administrator <kristofer.souza2027@hotmail.com>
2023-04-29 10:02:38 -0700

    Desafio Adicionado

---
+++ utilidadesCeV/dado/Desafio 111.py
@@ -0,0 +1,9 @@
+"""
+Exercício Python 110: Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(),
+que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.
+"""
+from utilidadesCeV.moeda.DesafioFunc_111 import resumo
+
+numero = float(input(' [?] - DIGITE UM VALOR E VEJA SEU:\n -> Dobro\n -> Metade\n -> + 10%\n -> - 10%\n [?] - R$'))
+resumo(numero)
+

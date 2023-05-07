2f9b508e2d615d3028d4f0fb4cee547feffd86a2
Administrator <kristofer.souza2027@hotmail.com>
2023-05-06 18:51:10 -0700

    [FASE DE APRIMORAMENTO] - Inserido tratamento de erros nas opções do usuário!

--- ConsultaDeMetas/ConsultaDeMetas.py
+++ ConsultaDeMetas/ConsultaDeMetas.py
@@ -1,5 +1,6 @@
 import time
 import datetime
+from modulo import tryOption
 
 # Cores
 red = '\033[31m'
@@ -9,27 +10,6 @@ ciano = '\033[36m'
 normal = '\033[m'
 roxo = '\033[35m'
 rosa = '\033[95m'
-
-
-def tryOption(option):
-    """
-    --> Função para determinar a opção que o usuário irá escolher no menu, atualmente temos 3 opções, de registro,
-    consulta ou de sair do programa, caso o usuário digitar outro número ou letras fora do esperado o sistema
-    apresentará uma exceção.
-    :param option: Entrada do usuário.
-    :return: Retorna a opção escolhida em caso de zero exceções.
-    """
-    try:
-        option = int(option)
-    except ValueError:
-        print(red + 'DIGITE UM NÚMERO INTEIRO VÁLIDO!' + normal)
-    except Exception as error:
-        print(red + f'ERRO DE {error.__class__}' + normal)
-    else:
-        if option in range(1, 4):
-            return option
-        else:
-            print(red + 'APENAS NÚMEROS INTEIROS DAS OPÇÕES LISTADAS!' + normal)
 
 
 def dateVerification():
---
+++ ConsultaDeMetas/modulo/__init__.py
@@ -0,0 +1,29 @@
+# Cores
+red = '\033[31m'
+green = '\033[32m'
+yellow = '\033[33m'
+ciano = '\033[36m'
+normal = '\033[m'
+roxo = '\033[35m'
+rosa = '\033[95m'
+
+
+def tryOption(option):
+    """
+    --> Função para determinar a opção que o usuário irá escolher no menu, atualmente temos 3 opções, de registro,
+    consulta ou de sair do programa, caso o usuário digitar outro número ou letras fora do esperado o sistema
+    apresentará uma exceção.
+    :param option: Entrada do usuário.
+    :return: Retorna a opção escolhida em caso de zero exceções.
+    """
+    try:
+        option = int(option)
+    except ValueError:
+        print(red + 'DIGITE UM NÚMERO INTEIRO VÁLIDO!' + normal)
+    except Exception as error:
+        print(red + f'ERRO DE {error.__class__}' + normal)
+    else:
+        if option in range(1, 4):
+            return option
+        else:
+            print(red + 'APENAS NÚMEROS INTEIROS DAS OPÇÕES LISTADAS!' + normal)

122e891b801347a4c34cb59bc9beb8f3fd17564b
Administrator <kristofer.souza2027@hotmail.com>
2023-05-11 16:26:14 -0700

    Desafio sendo realizado

--- Netflix_POO.py
+++ Netflix_POO.py
@@ -3,12 +3,45 @@ class Cliente:
         self.nome = nome
         self.idade = idade
         self.email = email
-        lista_planos = ["Basic", "Master"]
-        if plano in lista_planos:
+        self.lista_planos = ["Basic", "Master"]
+        if plano in self.lista_planos:
             self.plano = plano
         else:
-            raise Exception("[!] - PROCESSO INTERROMPIDO! (PLANO INEXISTENTE)")
+            raise Exception(f"[!] - PROCESSO INTERROMPIDO! (PLANO [{plano}] INEXISTENTE)")
+
+    def verFilme(self, filme):
+        return print(f'{cliente_id1.nome} está vendo o filme {filme}')
 
 
-cliente_id1 = Cliente("Kristofer", 21, "kristofer.souza@gmail.com", "Master")
+    def alterarPlano(self, novo_plano):
+        if novo_plano in self.lista_planos:
+            if novo_plano != self.plano:
+                try:
+                    self.plano = novo_plano
+                    print('Seu plano foi alterado com sucesso!')
+                except Exception as error:
+                    print(f'{error.__class__}')
+            else:
+                raise Exception(f"[!] - PROCESSO INTERROMPIDO! (PLANO [{novo_plano}] JÁ EM USO)")
+
+
+cliente_id1 = Cliente("Kristofer", 22, "kristofer.souza@gmail.com", "Master")
+
+
 cliente_id2 = Cliente("Jhonatam", 23, "jhonatam.souza@gmail.com", "Basic")
+
+print('{}'.format("CLIENTES".center(30)))
+print(f" Nome = {cliente_id1.nome}")
+print(f" Idade = {cliente_id1.idade}")
+print(f" Email = {cliente_id1.email}")
+print(f" Plano = {cliente_id1.plano}")
+print()
+print(f" Nome = {cliente_id2.nome}")
+print(f" Idade = {cliente_id2.idade}")
+print(f" Email = {cliente_id2.email}")
+print(f" Plano = {cliente_id2.plano}")
+print('\n\n\n\n\n')
+cliente_id1.verFilme("Homem Aranha")
+cliente_id1.alterarPlano("Basic")
+
+cliente_id2.verFilme("Homem De Ferro")

0976ba1fbe6e9e16ac858762e303a6afdebb830d
Administrator <kristofer.souza2027@hotmail.com>
2023-05-17 11:07:23 -0700

    Automatizando tarefas usando pyautogui

---
+++ TestandoNaAula.py
@@ -0,0 +1,11 @@
+import pyautogui
+import pandas as pd
+import time
+
+"""
+time.sleep(4)
+print(pyautogui.position())
+"""
+
+tabela = pd.read_csv(r"C:\Users\Administrator\Downloads\Compras.csv", sep=";")
+print(tabela)

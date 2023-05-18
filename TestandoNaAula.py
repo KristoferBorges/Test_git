5a34cb95ee66de94e8a8421395af7d93f9ff869a
Administrator <kristofer.souza2027@hotmail.com>
2023-05-18 09:37:23 -0700

    Atualizações com backup

--- TestandoNaAula.py
+++ TestandoNaAula.py
@@ -1,21 +1,5 @@
-from selenium import webdriver
 import pandas
-# import openpyxl
-# import numpy
-# import time
 
-tabela = pandas.read_excel(r"C:\Users\Administrator\Downloads\commodities.xlsx")
-
-for linha in tabela.index:
-
-    produto = tabela.loc[linha, "Produto"]
-    navegador = webdriver.Chrome()
-    navegador.get(f"https://www.melhorcambio.com/{produto}-hoje")
-    cotacao = navegador.find_element('xpath', '//*[@id="comercial"]').get_attribute('value')
-    cotacao = cotacao.replace(".", "").replace(",", ".")
-    tabela.loc[linha, 'Preço Atual'] = float(cotacao)
-
-
-tabela["Comprar"] = tabela["Preço Atual"] < tabela["Preço Ideal"]
+tabela = pandas.read_csv(r"C:\Users\Administrator\Desktop\ExerciciosGit\ConsultaDeMetas\listaRDMARCAS.txt", sep='|')
+tabela.to_excel("DadosExcel.xlsx", index=False)
 print(tabela)
-tabela.to_excel("CommoditiesATT.xlsx", index=False)

b53c4688398c5d4d597e83f845dce9be4f9af8c9
Administrator <kristofer.souza2027@hotmail.com>
2023-05-17 21:42:30 -0700

    Novo desafio adicionado

--- TestandoNaAula.py
+++ TestandoNaAula.py
@@ -1,11 +1,21 @@
-import pyautogui
-import pandas as pd
-import time
+from selenium import webdriver
+import pandas
+# import openpyxl
+# import numpy
+# import time
 
-"""
-time.sleep(4)
-print(pyautogui.position())
-"""
+tabela = pandas.read_excel(r"C:\Users\Administrator\Downloads\commodities.xlsx")
 
-tabela = pd.read_csv(r"C:\Users\Administrator\Downloads\Compras.csv", sep=";")
+for linha in tabela.index:
+
+    produto = tabela.loc[linha, "Produto"]
+    navegador = webdriver.Chrome()
+    navegador.get(f"https://www.melhorcambio.com/{produto}-hoje")
+    cotacao = navegador.find_element('xpath', '//*[@id="comercial"]').get_attribute('value')
+    cotacao = cotacao.replace(".", "").replace(",", ".")
+    tabela.loc[linha, 'Preço Atual'] = float(cotacao)
+
+
+tabela["Comprar"] = tabela["Preço Atual"] < tabela["Preço Ideal"]
 print(tabela)
+tabela.to_excel("CommoditiesATT.xlsx", index=False)

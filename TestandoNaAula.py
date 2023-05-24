133cb9777562bcb1f46a9bee2dba3140798e97e4
Administrator <kristofer.souza2027@hotmail.com>
2023-05-23 18:11:21 -0800

    TestesConstantes

--- TestandoNaAula.py
+++ TestandoNaAula.py
@@ -1,5 +1,25 @@
-import datetime
+from fastapi import FastAPI
 
-hora = datetime.datetime.now()
-hora = hora.strftime("%H;%M;%S")
-print(hora)
+app = FastAPI()
+
+# Rota para a página inicialde
+@app.get('/')
+def home():
+    return {'message': 'Bem-vindo à API básica!'}
+
+# Rota para retornar um JSON com dados
+@app.get('/api/dados')
+def get_dados():
+    dados = {
+        'nome': 'João',
+        'idade': 25,
+        'cidade': 'São Paulo'
+    }
+    return dados
+
+# Rota para receber dados via POST e retornar uma resposta
+@app.post('/api/receber')
+def receber_dados(dados: dict):
+    nome = dados['nome']
+    mensagem = f'Olá, {nome}! Dados recebidos com sucesso.'
+    return {'mensagem': mensagem}

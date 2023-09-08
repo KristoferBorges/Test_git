9ff0982f09160e24d6f79bc8b042386e02e67093
Administrator <kristofer.souza2027@hotmail.com>
2023-09-08 15:49:07 -0800

    Adicionado um formatador de CPF

--- functions/utilitarian.py
+++ functions/utilitarian.py
@@ -1,1 +1,5 @@
-print('hello world')
+class verificaCPF:
+    def capturaCPF(cpf):
+        cpf = cpf.replace('.', '').replace('-', '')
+        cpf = cpf[:3] + '.' + cpf[3:6] + '.' + cpf[6:9] + '-' + cpf[9:]
+        return print(f'Resutlado {cpf}')

d0ce63f4616f4a1149386807fc8c13134919aa6d
Administrator <kristofer.souza2027@hotmail.com>
2023-09-20 09:47:16 -0800

    Tabuada com função importada

---
+++ Tabuada.py
@@ -0,0 +1,7 @@
+from functions.utilitarian import Tabuada
+
+
+multiplicador = Tabuada.input_multiplicador_tabuada()
+
+for i in range(1, 10 + 1):
+    print(f'{multiplicador} x {i} = {multiplicador * i}')

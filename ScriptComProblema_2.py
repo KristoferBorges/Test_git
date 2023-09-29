d52e722d93e41c7e91c8daf45c0f0227fc5e6a30
Administrator <kristofer.souza2027@hotmail.com>
2023-09-28 21:04:00 -0800

    Exercício resolvido

---
+++ ScriptComProblema_2.py
@@ -0,0 +1,17 @@
+# Script Python com um problema
+
+def calcular_media(lista):
+    try:
+        total = 0
+        for numero in lista:
+            total += numero
+        media = total / len(lista)
+        return media
+    except ZeroDivisionError:
+        print("\nNão é possível calcular a média de uma lista vazia!")
+        return 0
+
+numeros = []
+
+media = calcular_media(numeros)
+print(f"A média dos números é: {media}")

18e7aefa9e5dd31fbc4261f003e6cd1eb612c734
Administrator <kristofer.souza2027@hotmail.com>
2023-10-20 17:25:37 -0800

    Teste em script

--- ScriptComProblema_2.py
+++ ScriptComProblema_2.py
@@ -11,7 +11,7 @@ def calcular_media(lista):
         print("\nNão é possível calcular a média de uma lista vazia!")
         return 0
 
-numeros = []
+numeros = [1,3,5,6]
 
 media = calcular_media(numeros)
 print(f"A média dos números é: {media}")

58453284abf4aeadcc8a6c66bc784b736c7c4d54
KristoferBorges <kristofer.souza2027@hotmail.com>
2023-11-26 13:39:37 -0800

    ATT

--- ScriptsExecutaveis/decifradorDeCPF/main.py
+++ ScriptsExecutaveis/decifradorDeCPF/main.py
@@ -1,18 +1,31 @@
 import random
+import time
 
-# Desafio 0
+# Cor verde
+green = "\033[0;32m"
+yellow = "\033[0;33m"
+red = "\033[0;31m"
+normal = "\033[0m" # to come back to default 
 
 print("====== DESAFIO 0 ======\n\n");
-
-cpfCorreto = "170.673.340-20"
+cpfCorreto = input(red + '\n[?] - DIGITE UM CPF PARA DECIFRAR: ' + normal)
 
 def hackearCPF(cpf):
-    print("[!] - CPF: " + cpf)
+    print(yellow + '[!] - COMEÇANDO A PROCURA EM' + green + ' 3 SEGUNDOS')
+    time.sleep(1)
+    print(yellow + '[!] - COMEÇANDO A PROCURA EM' + green + ' 2 SEGUNDOS')
+    time.sleep(1)
+    print(yellow + '[!] - COMEÇANDO A PROCURA EM' + green + ' 1 SEGUNDOS')
+    time.sleep(1)
 
+    print(yellow + "[!] - CPF: " + green + cpf)
     tentativas = 0
     cpfFalhos = list()
     maxTentativas = 99999
+
     while True:
+        
+
         n1 = random.randint(0, 9)
         n2 = random.randint(0, 9)
         n3 = random.randint(0, 9)
@@ -39,12 +52,12 @@ def hackearCPF(cpf):
         print(f'\n[!] - TENTATIVA = [{tentativas}] - [{maxTentativas}]')
         print(f'[INFO] - {cpfDecifrado}')
         if tentativas == maxTentativas:
-            print("\n[!] - CPF não decifrado")
+            print(red +"\n[!] - CPF não decifrado")
             print(f'[!] - TENTATIVAS = [{maxTentativas}]')
             break
         elif cpfCorreto == cpfDecifrado:
-            print(f'\n[!] - CPF DECIFRADO = [{cpfDecifrado}]')
-            print(f'[!] - TENTATIVAS FEITAS = [{tentativas}]')
+            print(green + f'\n[!] - CPF DECIFRADO = [{cpfDecifrado}]')
+            print(f'[!] - TENTATIVAS FEITAS = [{tentativas}]' + normal)
             break
         else:
             cpfFalhos.append(cpfDecifrado)
@@ -57,6 +70,6 @@ def hackearCPF(cpf):
     
 
 
-cpf = input("Digite um CPF: ")
+cpf = input(green + "[?] - DIGITE O CPF ESCONDIDO: " + normal)
 hackearCPF(cpf)
-end = input("\n\nPressione ENTER para sair...")
+end = input('[!] - DIGITE ENTER PARA FINALIZAR')
